8107de39ff302933aef71c6c2f53c13c235b2d9e
Arlys Andrade <arlysandrade@gmail.com>
2020-03-24 13:37:35 -0300

    Banco de dados H2 e criação automatica de base de dados

--- src/main/java/com/arlys/cursomc/domain/Categoria.java
+++ src/main/java/com/arlys/cursomc/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.arlys.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private Integer Id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	
 	public Categoria() {
@@ -14,16 +23,16 @@ public class Categoria implements Serializable {
 
 	public Categoria(Integer id, String nome) {
 		super();
-		Id = id;
+		this.id = id;
 		this.nome = nome;
 	}
 
 	public Integer getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Integer id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getNome() {
@@ -38,7 +47,7 @@ public class Categoria implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -51,10 +60,10 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
